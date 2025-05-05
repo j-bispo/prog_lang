4b577c611b77bb8e976da60732692335a4880115
j-bispo <jalberto.bispo@gmail.com>
2025-05-05 09:50:27 -0300

    check 1

---
+++ pratica6/src/main.c
@@ -0,0 +1,35 @@
+/*
+Program: 
+
+Author: 
+
+Date: 
+
+Description: 
+
+License: CC BY-SA
+*/
+
+// C code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./include/list.h"
+
+int main() {
+    tList list = create_list();
+
+    insert_list(&list, 10);
+    insert_list(&list, 20);
+    insert_list(&list, 30);
+
+    print_list(&list);
+
+    remove_list(&list, 20);
+
+    print_list(&list);
+
+    free_list(&list);
+
+    return 0;
+}
